6d2cc96954ff9bab9eb82fc993164673986ecfe2
richie-south <richard.soderman@gmail.com>
2026-04-22 09:15:11 +0200

    fix: send logger

--- src/lib/llmlingua-2/factory.ts
+++ src/lib/llmlingua-2/factory.ts
@@ -215,6 +215,8 @@ export async function WithXLMRoBERTa(
     get_pure_tokens_xlm_roberta_large,
     is_begin_of_new_word_xlm_roberta_large,
     oaiTokenizer,
+    undefined,
+    logger,
   );
 
   logger({ promptCompressor });
@@ -291,6 +293,8 @@ export async function WithBERTMultilingual(
     get_pure_tokens_bert_base_multilingual_cased,
     is_begin_of_new_word_bert_base_multilingual_cased,
     oaiTokenizer,
+    undefined,
+    logger,
   );
 
   logger({ promptCompressor });
